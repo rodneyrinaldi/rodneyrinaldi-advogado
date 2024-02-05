6dfcdc8bcb94318f693a9c56cd149f169cda60ab
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2024-02-05 10:15:31 -0300

    adding aerea pages

---
+++ src/pages/aerea.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Home from "../components/aerea"
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Index() {
+  return (
+      <Home />
+    )
+}
